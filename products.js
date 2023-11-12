333298891c127235a296b95c1411fd86fc6f6ca6
k-stern <93948876+k-stern@users.noreply.github.com>
2023-11-12 22:53:39 +0000

    added products file and checkout visibility

---
+++ products.js
@@ -0,0 +1,82 @@
+let products = [
+    {
+        id: 1001,
+        subject: "Math",
+        location: "London",
+        price: 2000,
+        availability: 5,
+        image: "assets/images/product-fullsize.png"
+    },
+    {
+        id: 1002,
+        subject: "English",
+        location: "London",
+        price: 2000,
+        availability: 5,
+        image: "assets/images/product-fullsize.png"
+    },
+    {
+        id: 1003,
+        subject: "Science",
+        location: "Harrow",
+        price: 2000,
+        availability: 5,
+        image: "assets/images/product-fullsize.png"
+    },
+    {
+        id: 1004,
+        subject: "Spanish",
+        location: "London",
+        price: 2000,
+        availability: 5,
+        image: "assets/images/product-fullsize.png"
+    },
+    {
+        id: 1005,
+        subject: "French",
+        location: "Edgware",
+        price: 2000,
+        availability: 5,
+        image: "assets/images/product-fullsize.png"
+    },
+    {
+        id: 1006,
+        subject: "History",
+        location: "Colindale",
+        price: 2000,
+        availability: 5,
+        image: "assets/images/product-fullsize.png"
+    },
+    {
+        id: 1007,
+        subject: "Law",
+        location: "Hendon",
+        price: 2000,
+        availability: 5,
+        image: "assets/images/product-fullsize.png"
+    },
+    {
+        id: 1008,
+        subject: "Business",
+        location: "Barnet",
+        price: 2000,
+        availability: 5,
+        image: "assets/images/product-fullsize.png"
+    },
+    {
+        id: 1009,
+        subject: "Economics",
+        location: "Camden",
+        price: 2000,
+        availability: 5,
+        image: "assets/images/product-fullsize.png"
+    },
+    {
+        id: 1010,
+        subject: "IT",
+        location: "Hackney",
+        price: 2000,
+        availability: 5,
+        image: "assets/images/product-fullsize.png"
+    }
+];
